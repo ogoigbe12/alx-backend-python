6bbe498981dc6c5d71eea565d3ed28986adb3cf1
ogoigbe12 <ogoigbe12@gmail.com>
2022-04-18 23:54:17 -0700

    random number 0-10 added with task 0-2

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+...
+Async Generator
+...
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+...
+yield a random number between 0 and 10.
+...
+for count in range(10):
+    await asyncio.sleep(1)
+    yield random.random() * 10
